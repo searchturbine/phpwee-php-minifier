7319f32d82944c5775d4c03d60e92bf31dc2b824
Carsten Henkelmann <chenkelmann@hygi.de>
2015-09-15 13:41:50 +0200

    Table structure fixed.

--- examples/example.php
+++ examples/example.php
@@ -1,9 +1,9 @@
-<?
-require_once ("../phpwee.php");
+<?php
+require_once("../PHPWee.php");
 
 $html = file_get_contents("http://en.wikipedia.org/wiki/Minification_%28programming%29");
 $minified = PHPWee\Minify::html($html);
-echo  print_performance_graph("Wikipedia",$minified,$html);
+print_performance_graph("Wikipedia", $minified, $html);
 
 /*
 $html = file_get_contents("http://www.codeproject.com/Articles/759094/Step-by-Step-PHP-Tutorials-for-Beginners-Creating");
@@ -28,11 +28,33 @@ print_performance_graph("SearchTurbine",$minified,$html);
 
 
 ///////////////////////////////////////////////////////////////
- 
 
-function print_performance_graph($subject,$minified,$html){
-	$before = strlen(gzcompress($html));
-	$after = strlen(gzcompress($minified));	
-	$improvement =  100 * (1-($after/$before));
-	echo  "<table style='width:100%; border:1px solid grey;text-align:center'><tr><th colspan=3><b>$subject</th><tr><th>Gzipped Bytes Before PHPWee</th><th>Gzipped Bytes After PHPWee</th><th>% Performance Boost</th></tr><tr><td>$before before</td><td>$after after</td><td> $improvement % faster </td></table><br><br>";
+/**
+ * @param $subject
+ * @param $minified
+ * @param $html
+ */
+function print_performance_graph($subject, $minified, $html)
+{
+    $before = strlen(gzcompress($html));
+    $after = strlen(gzcompress($minified));
+    $improvement =  100 * (1-($after/$before));
+    ?>
+	<table style="width:100%; border:1px solid grey;text-align:center">
+		<tr>
+			<th colspan="3"><?= $subject; ?></th>
+		</tr>
+		<tr>
+			<th>Gzipped Bytes Before PHPWee</th>
+			<th>Gzipped Bytes After PHPWee</th>
+			<th>% Performance Boost</th>
+		</tr>
+		<tr>
+			<td><?= $before; ?> before</td>
+			<td><?= $after; ?> after</td>
+			<td><?= $improvement; ?> % faster</td>
+		</tr>
+	</table>
+	<br><br>
+	<?php
 }
